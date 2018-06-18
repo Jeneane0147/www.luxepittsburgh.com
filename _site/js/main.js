4fd91c2e6ed7cb5ce78977a2666fc3faffaa4597
Jeneane <jeneane0147@aol.com>
2018-06-18 20:52:40 +0800

    hamburger menu and stuff

--- _site/js/main.js
+++ _site/js/main.js
@@ -4,7 +4,21 @@ $('document').ready(function(){
   // Add ScrollReveal
   window.sr = ScrollReveal();
   sr.reveal('.show-me');
-  
+
+  /*
+  ** Toggle's body class
+  ** change the CSS accordingly: body.menu-visible for a visible menu - body.menu-hidden for a hidden menu
+  */
+  function toggleMenu() {
+
+    // Change the body class
+    $('body').toggleClass('menu-visible menu-hidden');
+
+  }
+
+  // Bind click event to the hamburger button
+  $('body').on('click', '.toggle-menu', toggleMenu);
+
 });
 
 // Wait for images to load
