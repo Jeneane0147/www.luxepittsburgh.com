fcbd44b990d4e32ffc795057d04f0f2b511cc3e2
Jeneane <jeneane0147@aol.com>
2018-06-18 16:40:39 +0800

    services links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+// Wait for the dom to load
+$('document').ready(function(){
+
+  // Add ScrollReveal
+  window.sr = ScrollReveal();
+  sr.reveal('.show-me');
+
+});
+
+// Wait for images to load
 $(window).on('load', function(){
 
+  // Flexslider
   $('.flexslider').flexslider();
 
 });
